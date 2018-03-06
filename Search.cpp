881038f292b4e6bce995fc140646f7711606e35f
Limy612 <512901754@qq.com>
2018-03-06 17:47:26 +0800

    灌灌灌灌

---
+++ Search.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+//binary search
+int BinarySearch(int *a, int n, int key)
+{
+	return 0;
+}
+
+int main()
+{
+	int a[20] = {0,1,16,24,35,47,59,62,73,88,99};
+	int n = 11, key = 62, output;
+	output = BinarySearch(a,n,key);
+	cout << output;
+	
+	return 0;
+}
